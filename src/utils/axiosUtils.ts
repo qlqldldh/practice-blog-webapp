e3f27c4da265c8f30fa116dc87d3ff5077d243f0
Dean <qlqldldh@fadutec.com>
2021-03-28 23:23:50 +0900

    fix: remove a useless comment

--- src/utils/axiosUtils.ts
+++ src/utils/axiosUtils.ts
@@ -3,9 +3,7 @@ import axios from "axios";
 import { ContactlessOutlined } from '@material-ui/icons';
 
 export const fetchData = async (url: string, params?: any) => {
-    // const fullUrl = process.env.NEXT_PUBLIC_API_URL + url;
     const fullUrl = 'http://localhost:8000' + url;
-    console.log(fullUrl);
 
     const response = await axios.get(fullUrl, params)
                                 .then(response => response);
@@ -21,7 +19,6 @@ export const getItem = async (url: string, params?: any) => {
 
 export const getList = async (url: string, params?: any) => {
     const { data, status } = await fetchData(url, params);
-    console.log(data, status, typeof(data));
 
     return { data: data.map((item: any) => toCamelCaseKeys(item)), status };
 }
